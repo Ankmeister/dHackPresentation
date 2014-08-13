c395d0efa8728ff3844690b3b73dddbe3cd0bbaa
Zhewriix <hagmar.magnus@gmail.com>
2014-08-13 22:28:41 +0200

    Added some more descriptions. Even more are required

--- texts.py
+++ texts.py
@@ -10,13 +10,20 @@ intro_text.append( "jag heter kalle")
 intro_text.append( "jag heter kalle")
 
 hagge_text = []
-hagge_text.append(u"Andreas 'det blev ingen cd' ' 'lökhackaren' Hagesjö. Superdatordriftsystemman")
+hagge_text.append(u"Andreas 'Ankan' 'Hagge' 'det blev ingen cd' Hagesjö. Superdatordriftsystemman")
+hagge_text.append(u" - Kan inte hacka lök")
+hagge_text.append(u" - Får skulden")
 
 mort_text = []
 mort_text.append(u"Niclas 'Mort' Ogeryd. Största dickbutten. Mikrodatordriftsystemman")
+mort_text.append(u" - Latast i dHack. Han orkar inte ens skriva klart sina")
+mort_text.append(u" - Störst dickbutt")
 
 ninja_text = []
 ninja_text.append(u"Magnus 'ninja' 'MANGEEEEE' 'TJENNNNA' 'ircförstöraren' Hagmar. Mikrodatordriftsystemman")
+ninja_text.append(u" - Motto: \"Äh, det kan väl inte gå sönder om man-... Well shit\"")
+ninja_text.append(u" - Oftast ansvarig när något är trasigt")
 
 skurk_text = []
-skurk_text.append(u"Robert 'TJENNNNNNNNNNNNNNNNNNNA' Nyquist. Avgående mikrodatordriftsystemman")
+skurk_text.append(u"Robert 'skurk' 'TJENNNNNNNNNNNA'  Nyquist. Mikrodatordriftsystemman")
+skurk_text.append(u" - Har avgått ca 126 gånger")
