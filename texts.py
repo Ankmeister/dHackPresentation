23852a3371f32c31dd10a3292182be78f5c0f1b9
Magnus Hagmar <hagmar.magnus@gmail.com>
2014-08-18 01:34:06 +0200

    Changed the describing texts slightly

--- texts.py
+++ texts.py
@@ -12,18 +12,19 @@ intro_text.append( "jag heter kalle")
 hagge_text = []
 hagge_text.append(u"Andreas 'Ankan' 'Hagge' 'det blev ingen cd' Hagesjö. Superdatordriftsystemman")
 hagge_text.append(u" - Kan inte hacka lök")
-hagge_text.append(u" - Får skulden")
+hagge_text.append(u" - Får skulden när det skiter sig")
 
 mort_text = []
-mort_text.append(u"Niclas 'Mort' Ogeryd. Största dickbutten. Mikrodatordriftsystemman")
+mort_text.append(u"Niclas 'Mort' 'John Mortimer' Ogeryd. Största dickbutten. Mikrodatordriftsystemman")
 mort_text.append(u" - Latast i dHack. Han orkar inte ens skriva klart sina")
 mort_text.append(u" - Störst dickbutt")
+mort_text.append(u" - Alltid in-game Dota 2 på Steam")
 
 ninja_text = []
-ninja_text.append(u"Magnus 'ninja' 'MANGEEEEE' 'TJENNNNA' 'ircförstöraren' Hagmar. Mikrodatordriftsystemman")
-ninja_text.append(u" - Motto: \"Äh, det kan väl inte gå sönder om man-... Well shit\"")
+ninja_text.append(u"Magnus 'ninja' 'MANGEEEEE' 'coolboi69' Hagmar. Mikrodatordriftsystemman")
+ninja_text.append(u" - \"Äh, det kan väl inte gå sönder om man-... Well shit\"")
 ninja_text.append(u" - Oftast ansvarig när något är trasigt")
 
 skurk_text = []
-skurk_text.append(u"Robert 'skurk' 'TJENNNNNNNNNNNA'  Nyquist. Mikrodatordriftsystemman")
+skurk_text.append(u"Robert 'skurk' 'Bobby'  Nyquist. Mikrodatordriftsystemman")
 skurk_text.append(u" - Har avgått ca 126 gånger")
