c840c9a05e7fee45fad03cf90b6c8db22aa110a6
Magnus Hagmar <hagmar.magnus@gmail.com>
2014-08-18 17:18:58 +0200

    ohgod

--- main.py
+++ main.py
@@ -49,23 +49,46 @@ def delete_random_letter(l, count):
 	l[indices[randint(0,len(indices) - 1)]].pop()
 	delete_random_letter(l, count - 1)
 	
-def draw_ascii_pictures(text, (x, y), font=monospace6):
+def draw_ascii_pictures(text, (x, y), delay, font=monospace6):
 	"""Draws asciipicture one letter at a time"""
 	dickbutt = [0]*len(text)
 	rowIndices = range(len(text))
+	fontwidth = font.size("O")[0]
+	fontheight = font.get_height()
 	while rowIndices:
 		index = randint(0, len(rowIndices)-1)
-		letterToRender = "".join(text[index][dickbutt[index]])
-		screen.blit(font.render(letterToRender ,1, ORANGE),(x+font.size(text[0][0])[0]*dickbutt[index],y+font.get_height()*rowIndices[index]))
-		screen.blit(font.render(letterToRender ,1, ORANGE),(x+font.size(text[0][0])[0]*dickbutt[index],y+font.get_height()*rowIndices[index]))
-		screen.blit(font.render(letterToRender ,1, ORANGE),(x+font.size(text[0][0])[0]*dickbutt[index],y+font.get_height()*rowIndices[index]))
+		letterToRender = text[index][dickbutt[index]]
+		xpos = x+fontwidth*dickbutt[index]
+		ypos = y+font.get_height()*rowIndices[index]
+		screen.blit(font.render(letterToRender ,1, ORANGE),(xpos,ypos))
+		screen.blit(font.render(letterToRender ,1, ORANGE),(xpos,ypos))
+		screen.blit(font.render(letterToRender ,1, ORANGE),(xpos,ypos))
 		dickbutt[index] += 1
-		if dickbutt[index] == len(text[0])-1:
+		if dickbutt[index] >= len(text[0])-1:
 			rowIndices.pop(index)
 			dickbutt.pop(index)
-		pygame.display.flip()
+		pygame.display.update(pygame.Rect(xpos, ypos, fontwidth, fontheight))
+		timenow = time()
+		uglyDelay(delay)
 	
 	return y+font.get_height()*len(text)
+		
+
+def uglyDelay(delay):
+	for thisCodeIsHorrible in range(delay):
+		continue
+	
+def draw_ascii_pictures2(mort, (x,y), font=monospace6):
+	fontwidth = font.size("H")[0]
+	fontheight = font.get_height()
+	for tjenna in range(len(mort[0])):
+		for dickbutt in range(len(mort)):
+			xpos = x+fontwidth*tjenna
+			ypos = y+fontheight*dickbutt
+			screen.blit(font.render(mort[dickbutt][tjenna] ,1, ORANGE),(xpos,ypos))
+		pygame.display.flip()
+		sleep(0.1)
+
 
 def blit_asciipic(asciipic, (x,y),font=monospace6):
 	for i in asciipic:
@@ -73,19 +96,20 @@ def blit_asciipic(asciipic, (x,y),font=monospace6):
 		y += font.get_height()
 
 def fade_in(pic, pos, adddelay=0):
-	for t in range(80):
+	for t in range(100):
 		pic.set_alpha(t)
 		screen.blit(pic,pos)
 		pygame.display.flip()
 		sleep(0.02+adddelay)
 	sleep(1)
 
-def fade_to_black():
+def fade_to_black(delay=0):
 	blackground = pygame.Surface(screen.get_size())
 	for i in range(100): #apparantly, 100 is enough.
 		blackground.set_alpha(i)
 		screen.blit(blackground,(0,0))
 		pygame.display.flip()
+		sleep(delay)
 
 def fade_pic_to_ascii(pic,asciipic,pos):
 	fade_in(pic,pos)
@@ -127,9 +151,9 @@ def from_asciipic_to_realtext(pic, asciipic, text, (textx, texty), asciipos, add
 	fade_to_black()
 	
 	
-def asciipic_to_real_pic(pic,asciipic, picpos):
+def asciipic_to_real_pic(pic,asciipic, picpos, delay):
 	"""Animates an asciipic by writing one letter at a time, and then fades the finished asciipicture into pic"""
-	bottomPic = draw_ascii_pictures(asciipic,picpos)
+	bottomPic = draw_ascii_pictures(asciipic,picpos, delay)
 	fade_in(pic, picpos, 0.01)
 	sleep(1)
 	dHack = "TJENNA"
@@ -142,7 +166,49 @@ def fade_pic_to_real_text(pic, asciipic, picpos, text, textpos, adddelay=0):
 	fade_pic_to_ascii(pic, asciipic, picpos)
 	from_asciipic_to_realtext(pic, asciipic, text, textpos, picpos, adddelay)
 
+def asciipic_to_real_pic_with_text(picture, asciitext, (x,y), infotext, textpos, delay, font=monospace6, textfont=monospace20):
+	total_letters_in_pic = len(asciitext[0]) * len(asciitext)
+	total_letters_in_text = sum([len(a) for a in infotext])
+	ratio = total_letters_in_pic/total_letters_in_text
 	
+	dickbutt = [0]*len(asciitext)
+	rowIndices = range(len(asciitext))
+	fontwidth = font.size("O")[0]
+	fontheight = font.get_height()
+	
+	while 
+	while rowIndices:
+		index = randint(0, len(rowIndices)-1)
+		letterToRender = asciitext[index][dickbutt[index]]
+		xpos = x+fontwidth*dickbutt[index]
+		ypos = y+font.get_height()*rowIndices[index]
+		screen.blit(font.render(letterToRender ,1, ORANGE),(xpos,ypos))
+		screen.blit(font.render(letterToRender ,1, ORANGE),(xpos,ypos))
+		screen.blit(font.render(letterToRender ,1, ORANGE),(xpos,ypos))
+		dickbutt[index] += 1
+		if dickbutt[index] >= len(asciitext[0])-1:
+			rowIndices.pop(index)
+			dickbutt.pop(index)
+		pygame.display.update(pygame.Rect(xpos, ypos, fontwidth, fontheight))
+		timenow = time()
+		uglyDelay(delay)
+	
+	fade_to_black(0.01)
+
+def draw_pic_letter(rowIndices, dickbutt, asciitext, (x,y), fontwidth, fontheight):
+	index = randint(0, len(rowIndices)-1)
+	letterToRender = asciitext[index][dickbutt[index]]
+	xpos = x+fontwidth*dickbutt[index]
+	ypos = y+font.get_height()*rowIndices[index]
+	screen.blit(font.render(letterToRender ,1, ORANGE),(xpos,ypos))
+	screen.blit(font.render(letterToRender ,1, ORANGE),(xpos,ypos))
+	screen.blit(font.render(letterToRender ,1, ORANGE),(xpos,ypos))
+	dickbutt[index] += 1
+	if dickbutt[index] >= len(asciitext[0])-1:
+		rowIndices.pop(index)
+		dickbutt.pop(index)
+	pygame.display.update(pygame.Rect(xpos, ypos, fontwidth, fontheight))
+	uglyDelay(delay) # Maybe?
 
 def main():
 	logo = pics.logo
@@ -158,14 +224,17 @@ def main():
 	haggepic = pygame.transform.scale(pygame.image.load('ingencd.png'), (get_picsize_from_asciipic(hagge))).convert()
 
 	pygame.mixer.music.play(-1)
+	#fade_pic_to_real_text(haggepic, hagge, center_pic(haggepic, 0, -150), hagge_text, (center_text(hagge_text), 600), 0.01)
+	asciipic_to_real_pic_with_text(haggepic, hagge, center_pic(haggepic, 0, -150), hagge_text, (center_text(hagge_text), 600), 5000)
+	asciipic_to_real_pic(ninjapic, ninja, center_pic(ninjapic, 0, -150), 5000)
+	asciipic_to_real_pic(skurkpic, skurk, center_pic(skurkpic, 0, -150), 5000)
+	asciipic_to_real_pic(mortpic, mort, center_pic(mortpic, 0, -150), 5000) 
 	
-	fade_pic_to_real_text(haggepic, hagge, center_pic(haggepic, 0, -150), hagge_text, (center_text(hagge_text), 600), 0.01)
+	#fade_pic_to_real_text(ninjapic, ninja, center_pic(ninjapic, 0, -140), ninja_text, (center_text(ninja_text), 650))
 
-	fade_pic_to_real_text(ninjapic, ninja, center_pic(ninjapic, 0, -140), ninja_text, (center_text(ninja_text), 650))
+	#fade_pic_to_real_text(skurkpic, skurk, center_pic(skurkpic, 0, -150), skurk_text, (center_text(skurk_text), 650), 0.015)
 
-	fade_pic_to_real_text(skurkpic, skurk, center_pic(skurkpic, 0, -150), skurk_text, (center_text(skurk_text), 650), 0.025)
-
-	fade_pic_to_real_text(mortpic, mort, center_pic(mortpic, 0, -150), mort_text, (center_text(mort_text), 580), 0.005)
+	#fade_pic_to_real_text(mortpic, mort, center_pic(mortpic, 0, -150), mort_text, (center_text(mort_text), 580), 0.005)
 	
 	asciipic_to_real_pic(dHack, logo, center_pic(dHack))
 	
